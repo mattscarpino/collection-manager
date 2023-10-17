97c298e5f71142a967ac355abaa3747694b4a1e1
Michael McGinn <mcginnm7@miamioh.edu>
2023-10-17 17:45:27 -0400

    We made the tables

---
+++ create.sql
@@ -0,0 +1,45 @@
+Drop Table if Exists game_console;
+Drop Table if Exists video_games;
+Drop Table if Exists genre;
+Drop Table if Exists console;
+ 
+CREATE TABLE genre(
+genreid Serial PRIMARY KEY,
+genrename VarChar(50)
+);
+
+CREATE TABLE Video_Games (
+gameid Serial PRIMARY KEY,
+video_game_name VarChar(50),
+Description VarChar(100),
+genreid Int References Genre(GenreID)
+);
+
+CREATE TABLE Console (
+consoleid Serial PRIMARY KEY,
+console_name VarChar(50),
+manufacturer VarChar(50)
+);
+
+CREATE TABLE Game_Console (
+consoleid Int References Console(consoleid),
+gameid Int References Video_Games(gameid),
+CONSTRAINT game_conrole_pk PRIMARY KEY (consoleid, videogameid)
+);
+
+INSERT INTO genre (
+genrename
+)
+VALUES ('Horror');
+
+INSERT INTO video_games (
+video_game_name,
+description
+)
+VALUES ('CallofDuty', 'First Person Shooter');
+
+INSERT INTO console (
+console_name, 
+manufacturer
+)
+VALUES ('PS5', 'Sony');
