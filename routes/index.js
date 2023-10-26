8753b652d1d83756011b21afa3092d584eec75bd
Michael McGinn <mcginnm7@miamioh.edu>
2023-10-26 17:31:32 -0400

    part 4

--- routes/index.js
+++ routes/index.js
@@ -8,6 +8,22 @@ router.get('/', async (req, res) => {
   let collectibles =
     'SELECT video_game_name, genrename, developer_name, console_name FROM video_games, genres, developers, consoles WHERE video_games.genreid = genres.genreid AND video_games.developerid = developers.developerid AND video_games.consoleid = consoles.consoleid';
 
+  const params = [];
+
+  if (req.query.video_game_name) {
+    collectibles += ' AND video_game_name ILIKE $1';
+    params.push('%' + req.query.video_game_name + '%');
+  } else if (req.query.genrename) {
+    collectibles += ' AND genrename ILIKE $1';
+    params.push('%' + req.query.genrename + '%');
+  } else if (req.query.console_name) {
+    collectibles += ' AND console_name ILIKE $1';
+    params.push('%' + req.query.console_name + '%');
+  } else if (req.query.developer_name) {
+    collectibles += ' AND developer_name ILIKE $1';
+    params.push('%' + req.query.developer_name + '%');
+  }
+
   if (req.query.sort === 'video_game_name_a-z') {
     collectibles += ' ORDER BY video_game_name';
   } else if (req.query.sort === 'genrename_a-z') {
@@ -28,7 +44,7 @@ router.get('/', async (req, res) => {
     collectibles += ' ORDER BY developer_name DESC';
   }
 
-  const result = await db.query(collectibles);
+  const result = await db.query(collectibles, params);
 
   res.render('index', { title: 'Express', collectibles: result.rows });
 });
