c4a3e51aa90b7b7827d50241284076eb24125f08
mattscarpino <scarpimc@miamioh.edu>
2023-10-31 18:02:27 -0400

    part 5

--- routes/index.js
+++ routes/index.js
@@ -49,6 +49,43 @@ router.get('/', async (req, res) => {
   res.render('index', { title: 'Express', collectibles: result.rows });
 });
 
+// router.get('/gamepage/:video_game_name', async (req, res) => { });
+
+// router.get('/add-new', async (req, res) => {
+//   res.render('add-new');
+// });
+
+router.post('/', async (req, res) => {
+  const query =
+    "INSERT INTO developers(developer_name) VALUES('" +
+    req.body.developer +
+    "');";
+  // const dubs = [];
+  // dubs.push('' + req.body.developer + '');
+
+  await db.query(query);
+
+  const second =
+    "INSERT INTO video_games(video_game_name, genreid, consoleid, developerid) VALUES('" +
+    req.body.game_name +
+    "', (SELECT genreid FROM genres WHERE genrename = '" +
+    req.body.genre +
+    "'), (SELECT consoleid FROM consoles WHERE console_name = '" +
+    req.body.console +
+    "'), (SELECT developerid FROM developers WHERE developer_name = '" +
+    req.body.developer +
+    "'));";
+
+  await db.query(second);
+
+  const collectibles =
+    'SELECT video_game_name, genrename, developer_name, console_name FROM video_games, genres, developers, consoles WHERE video_games.genreid = genres.genreid AND video_games.developerid = developers.developerid AND video_games.consoleid = consoles.consoleid';
+
+  const reload = await db.query(collectibles);
+
+  res.render('index', { title: 'Express', collectibles: reload.rows });
+});
+
 // Listing collectibles
 
 module.exports = router;
